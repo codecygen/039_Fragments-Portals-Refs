ba62fe05a02bffdad7fcde4d5ea276b0eee296c8
gingercook <vahitaras@gmail.com>
2021-12-02 22:02:19 -0500

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Fragments, Portals, Refs
+#Fragments, Portals, Refs
+
+
+##Fragments
+
+
+Ref hook is different than State hook. If you just want to read an element, Ref hook has less code. If you also want to re-render something, you need to use State hook.
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,41 +6,59 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  // Using useRef
   const nameInputRef = useRef();
   const ageInputRef = useRef();
+  // Using useRef
 
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  // const [enteredUsername, setEnteredUsername] = useState('');
+  // const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    // Using useRef
+    const enteredUsernameRef = nameInputRef.current.value;
+    const enteredAgeRef = ageInputRef.current.value;
+    // Using useRef
+
+    // Using useRef
+    if (enteredUsernameRef.trim().length === 0 || enteredAgeRef.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+
+    // Using useRef
+    if (+enteredAgeRef < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+
+    // Using useRef
+    props.onAddUser(enteredUsernameRef, enteredAgeRef);
+
+    // setEnteredUsername('');
+    // setEnteredAge('');
+
+    // This is uncontrolled component access.
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
@@ -61,17 +79,19 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            // value={enteredUsername}
+            // onChange={usernameChangeHandler}
+            // Using useRef
             ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-            ref={nameInputRef}
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
+            // Using useRef
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
